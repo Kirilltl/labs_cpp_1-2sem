37c831b420ac8a743236a17065f8e67ccd8de7b3
Kirilltl <kyzmenko20047@mail.ru>
2020-09-22 22:03:20 +0300

    peredelal lab3

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -3,28 +3,20 @@
 int f(int a, int b)
 {
 	int result = 1;
-
-	for (int i = 1; i < 10; i++)
+for (int i = 1; i < 10; i++)
 	{
-	    result = result * b;
+		result = result * b;
 		if (result == a)
 		{
 			return i;
 		}
 	}
-	
-	
-	
-	return -1;
+return -1;
 }
-
-
-int main() 
+int main()
 {
 	printf("%d\n", f(25, 5));
 	printf("%d\n", f(27, 3));
 	printf("%d\n", f(23, 5));
 	printf("%d\n", f(128, 2));
-
-
 }

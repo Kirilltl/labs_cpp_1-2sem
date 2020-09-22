6b979209ad605aa2381eb76750d2ab80336a4d7c
Kirilltl <kyzmenko20047@mail.ru>
2020-09-22 20:53:31 +0300

    add lab 1(new)

---
+++ lab 1(new)/lab 1(new)/Source.cpp
@@ -0,0 +1,31 @@
+# include <stdio.h>
+int main()
+{
+	double nums[10] = { -1.9993545, 2.5654, 5.6575, 2.34, -4.222, 3.45, 3.555, 7.787, 1.367, 5.56 };
+
+	for (int i = 0; i < 10; i++)
+	{
+		int cell = (int)nums[i]; // izmenenie tipa s drobogo na celoe
+
+
+		nums[i] = nums[i] - cell; // ubrali celuyu chast'
+		if (nums[i] < 0)
+		{
+			nums[i] = 10;
+		}
+		printf("%f  ---  %d \n", nums[i], cell);
+	}
+
+	// poisk minimal'noj drobnoj chasti
+	double min = 1;
+	for (int i = 0; i < 10; i++)
+	{
+
+		if (nums[i] < min)
+		{
+			min = nums[i];
+		}
+	}
+	printf("mini %f", min);
+	return 0;
+}

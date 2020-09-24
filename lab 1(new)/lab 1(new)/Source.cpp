7ed837b03d9c55e86b3dcc0963a38394e90f0e91
Kirilltl <kyzmenko20047@mail.ru>
2020-09-24 09:57:55 +0300

    change lab 1 new

--- lab 1(new)/lab 1(new)/Source.cpp
+++ lab 1(new)/lab 1(new)/Source.cpp
@@ -1,17 +1,20 @@
 # include <stdio.h>
 int main()
 {
-	double nums[10] = { -1.9993545, 2.5654, 5.6575, 2.34, -4.222, 3.45, 3.555, 7.787, 1.367, 5.56 };
+	float nums[10] = { -1.9993545, 2.5654, 5.6575, 2.34, -4.222, 3.45, 3.555, 7.787, 1.367, 5.56 };
+
 
 	for (int i = 0; i < 10; i++)
 	{
+	//	scanf("%f", &nums[i]);
+
 		int cell = (int)nums[i]; // izmenenie tipa s drobogo na celoe
 
 
 		nums[i] = nums[i] - cell; // ubrali celuyu chast'
 		if (nums[i] < 0)
 		{
-			nums[i] = 10;
+			nums[i] = -nums[i];
 		}
 		printf("%f  ---  %d \n", nums[i], cell);
 	}

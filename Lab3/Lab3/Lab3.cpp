5be03d2ef7cefbbabbe9d97a4b8a7bf39476ac9c
Kirilltl <kyzmenko20047@mail.ru>
2020-09-26 21:15:21 +0300

    change labs

--- Lab3/Lab3/Lab3.cpp
+++ Lab3/Lab3/Lab3.cpp
@@ -1,29 +1,23 @@
 #include <stdio.h>
-
-int f(int a, int b)
-{
+int f(int a, int b){
 	int result = 1;
-for (int i = 1; i < 10; i++)
-	{
-		result = result * b;
-		if (result == a)
+for (int i = 1; i < 20; i++){
+		result = result * a;
+		if (result == b)
 		{
 			return i;
 		}
 	}
-return -1;
+      return -1;
 }
-
-
-int main()
-{
-	int b=0;
+int main(){
 	int a = 0;
+	int b = 0;
 	scanf("%d", &a);
-	scanf("%d",&b );
-
-
-	if (f(a, b) >= 0)
-		printf("f(%d,%d)= %d\n", a, b, f(a, b));
-	else printf(" This f(%d,%d) does not have a degree\n",a,b);
-}
+	scanf("%d", &b);
+     if (f(a, b) >= 0)
+		printf("%d ^ %d = %d\n",a,f(a, b),b);
+	 else if (b == 1) 
+		 printf("%d ^ 0 = %d \n",a,b);
+	 else printf("%d ^ not degree = %d\n",a,b);
+	}

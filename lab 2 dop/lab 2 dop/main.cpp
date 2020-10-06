626824435aa84cdb784552e7f66f4c6c94fa1012
Alexander <kyzmenkokirill@mail.ru>
2020-10-06 11:19:29 +0300

    lab 2 dop

---
+++ lab 2 dop/lab 2 dop/main.cpp
@@ -0,0 +1,70 @@
+#include<stdio.h>
+#include <stdlib.h>
+#include<string.h>
+
+/*
+int comparesl(const char* str1, const char* str2)
+{
+	const unsigned char* s1 = (const unsigned char*)str1;
+	const unsigned char* s2 = (const unsigned char*)str2;
+
+	unsigned char c1 = (unsigned char)*s1;
+	unsigned char c2 = (unsigned char)*s2;
+
+	while (c1 == c2)
+	{
+
+		if (c1 == '\0')
+			return c1 - c2;
+
+		*s1++;
+		*s2++;
+
+		c1 = (unsigned char)*s1;
+		c2 = (unsigned char)*s2;
+	}
+	return c1 - c2;
+}*/
+
+int alfasort(const void* slovo1, const void* slovo2) // compare two objects
+{
+	return strcmp(*(char**)slovo1, *(char**)slovo2); // compare two strings: return 0,1,-1 
+	//return comparesl(*(char**)slovo1, *(char**)slovo2);
+
+	// char** - это указатель на char*
+	// *slovo1 - это значение этого указателя на char*
+
+}
+
+
+int main()
+{
+	char mass[100] = "hello goodby dog cat car";
+
+
+	char* uknaslo[100]; // ykazatel on single words 
+	int che = 0;// number of words
+
+	uknaslo[0] = &mass[0];
+	che++;
+	for (int i = 0; mass[i] != '\0'; i++)// simvol konza stroki
+	{
+		if (mass[i] == ' ')
+		{
+			mass[i] = '\0';// razdel'aem stroky na otdeln slova
+			uknaslo[che] = &mass[i + 1];
+			che++;
+		}
+	}
+
+
+
+	printf("che=%d\n", che);
+
+	qsort(uknaslo, che, sizeof(char*), alfasort);
+
+
+	for (int i = 0; i < che; i++)
+		printf("%s\n", uknaslo[i]);
+		
+}

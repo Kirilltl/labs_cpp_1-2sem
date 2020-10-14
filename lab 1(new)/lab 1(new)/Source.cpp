f44c1010dee642e6ea04edae09bf16a50d95406a
Alexander <kyzmenkokirill@mail.ru>
2020-10-14 21:20:07 +0300

    watch lab 1

--- lab 1(new)/lab 1(new)/Source.cpp
+++ lab 1(new)/lab 1(new)/Source.cpp
@@ -2,14 +2,16 @@
 # include <math.h>
 int main()
 {
-    float nums[10] = { -1.99, 2.565,-0.3, 2.34, -4.222, 3.45, 3.555,1, 1.367, 5.56 };
+    float nums[10] = { -1.99, 2.565,-0.3, 2.34, -4.222, 3.45, 3.555,1.3, 1.367, 5.56 };
         for (int i = 0; i < 10; i++)
 	  {
 		nums[i] = nums[i] - floor(nums[i]);
 		printf("drob. chast: %f \n",nums[i]);
-      }
+			
 
-	       // poisk minimal'noj drobnoj chasti
+
+      }
+                  // poisk minimal'noj drobnoj chasti
 	double min = 1;
 	for (int i = 0; i < 10; i++)
 	{

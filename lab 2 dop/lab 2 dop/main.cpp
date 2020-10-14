2d2eb17f51ec08095f2d75e37f2742dc3a31595e
Alexander <kyzmenkokirill@mail.ru>
2020-10-14 23:14:31 +0300

    lab 2 dop change

--- lab 2 dop/lab 2 dop/main.cpp
+++ lab 2 dop/lab 2 dop/main.cpp
@@ -1,47 +1,49 @@
 #include<stdio.h>
 #include <stdlib.h>
 #include<string.h>
-
- 
-// s1 ukaz na 1-u bukv v 1-m slov;s2 ukaz na 1-u bukv vo 2-m slov
-int comparesl(char* s1, char* s2)
+int comparesl(char* s1, char* s2)  // s1 ukaz na 1-u bukv v 1-m slov;s2 ukaz na 1-u bukv vo 2-m slov
 {
 	while (1)               // beskonech cikl
 		{
-			char c1 = *s1;     // c1 - bukv, na kotoruyu ukaz s1
-			char c2 = *s2;     // c2 - bukv, na kotoruyu ukaz s2
+			char c1 = *s1;     // c1 - bukv, na kot ukaz s1
+			char c2 = *s2;     // c2 - bukv, na kot ukaz s2
 				if (c1 == '\0')    // esli dostignut konec slova, vyxodim iz cikla
 					return c1 - c2;
 				if (c1 != c2)      // esli bukvy raznye, vyxodim iz cikla
 					return c1 - c2;
-			*s1++;             // dvigaem ukazatel na sleduyuschuyu bukvu  v 1-m slove
-			*s2++;             // dvigaem ukazatel na sleduyuschuyu bukvu vo 2-m slove
+			*(s1++);             // dvigaem ukazatel na sleduyuschuyu bukvu  v 1-m slove
+		    *(s2++);             // dvigaem ukazatel na sleduyuschuyu bukvu vo 2-m slove
 		}
 }
 int alfasort(const void* slovo1, const void* slovo2) // compare two objects
 {
-	//return strcmp(*(char**)slovo1, *(char**)slovo2); // compare two strings: return 0,1,-1 
 	return comparesl(*(char**)slovo1, *(char**)slovo2);
 }
 int main()
 {
-char simv[] = {',' ,'.'};
-char mass[100] = "does, goodby. dog.. cat.... car";
-char* uknaslo[100]; // ykazatel on single words 
+char mass[] = "does , goodby . dog .. cat ... car";
+char* uknaslo[30]; // ykazatel on single words 
 int che = 0;// number of words
 uknaslo[0] = &mass[0];
 che++;
-	for (int i = 0; mass[i] != '\0'; i++)// simvol konza stroki
+for (int i = 0; mass[i] != '\0'; i++)// simvol konza stroki
 		{
-			if ((mass[i] == ' ') || (mass[i] == simv[i]))
+			if ((mass[i] == ' ') || (mass[i] == ',') || (mass[i] == '.'))
 				{
 					mass[i] = '\0';// razdel'aem stroky na otdeln slova
-					uknaslo[che] = &mass[i + 1];
+					uknaslo[che] = &mass[i+1];
 					che++;
 				}
-		}
-printf("che=%d\n", che);
+			if ((mass[i] == ',') || (mass[i] == '.'))
+			{
+				',' == '\n';
+				'.' == '\n ';
+			}
+
+			
+	    }
+printf("che=%d", che);
 qsort(uknaslo, che, sizeof(char*), alfasort);
 	for (int i = 0; i < che; i++)
-		printf("%s\n", uknaslo[i]);
+		printf("%s \n", uknaslo[i]);
 }

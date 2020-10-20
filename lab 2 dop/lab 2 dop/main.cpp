1ca762cba53af6ab5849b0d7dae974416b8f23c7
Kirill Kyzmenko <71287749+Kirilltl@users.noreply.github.com>
2020-10-20 11:16:28 +0300

    as

--- lab 2 dop/lab 2 dop/main.cpp
+++ lab 2 dop/lab 2 dop/main.cpp
@@ -1,6 +1,5 @@
 #include<stdio.h>
-#include <stdlib.h>
-#include<string.h>
+
 
 int comparesl(char* s1, char* s2)  // s1 ukaz na 1-u bukv v 1-m slov;s2 ukaz na 1-u bukv vo 2-m slov
 {
@@ -16,28 +15,63 @@ int comparesl(char* s1, char* s2)  // s1 ukaz na 1-u bukv v 1-m slov;s2 ukaz na 
 		    *s2++;             // dvigaem ukazatel na sleduyuschuyu bukvu vo 2-m slove
 		}
 }
-int alfasort(const void* slovo1, const void* slovo2) // compare two objects
+
+
+void qsort(char* uknaslo[], int che)
 {
-	return comparesl(*(char**)slovo1, *(char**)slovo2);
+	
+	for (int j = 0; j < (che - 1); j++)// ne vyti za granicy mass
+	{
+		for (int i = 0; i < (che - 1); i++)
+		{
+			int result = comparesl(uknaslo[i], uknaslo[i + 1]); // vozvrashaet chislo 
+			//printf("%s %s  ->  %d \n", uknaslo[i], uknaslo[i+1], result);
+			char* temp;
+			if (result > 0) // nado pomenjat slova mestami
+			{
+				temp = uknaslo[i];
+				uknaslo[i] = uknaslo[i + 1];
+				uknaslo[i + 1] = temp;
+			}
+			//printf("%s %s  changed! \n", uknaslo[i], uknaslo[i + 1]);
+		}
+	}
+
+
+
+
 }
+
+
+
+
 int main()
 {
-char mass[100] = "does. goodby. dog.. cat.. car";
+char mass[] = "does goodby.,. dog., cat car";
 char* uknaslo[100]; // ykazatel on single words 
 int che = 0;// number of words
 uknaslo[0] = &mass[0];
 che++;
 for (int i = 0; mass[i] != '\0'; i++)// simvol konza stroki
 {
-	if ((mass[i] == ' ') || (mass[i] ==  '.'))
+	if ((mass[i] == ' ') || (mass[i] ==  '.') || (mass[i] == ','))
 	{
-		mass[i] = '\0';// razdel'aem stroky na otdeln slova
-		uknaslo[che] = &mass[i + 1];
+		mass[i] = '\0'; // razdel'aem stroky na otdeln slova
+		uknaslo[che] = &mass[i + 1]; // shob nachalo slovo c bukv a ne probela
 		che++;
 	}
 }
 printf("che=%d \n", che);
-qsort(uknaslo, che, sizeof(char*), alfasort);
-	for (int i = 0; i < che; i++)
+qsort(uknaslo, che);
+	
+
+for (int i = 0; i < che; i++)
+{
+	if (uknaslo[i][0] != '\0')
 		printf("%s \n", uknaslo[i]);
+}
+
+
+
+
 }

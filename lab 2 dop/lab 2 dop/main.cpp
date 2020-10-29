50159b3e4e30e3322bcf729888397e9090b3ba9d
Kirill Kyzmenko <71287749+Kirilltl@users.noreply.github.com>
2020-10-29 13:34:57 +0300

    correct lab 2 dop

--- lab 2 dop/lab 2 dop/main.cpp
+++ lab 2 dop/lab 2 dop/main.cpp
@@ -1,77 +1,56 @@
 #include<stdio.h>
-
-
-int comparesl(char* s1, char* s2)  // s1 ukaz na 1-u bukv v 1-m slov;s2 ukaz na 1-u bukv vo 2-m slov
+int comparesl(char* s1, char* s2)  
 {
-	while (1)               // beskonech cikl
+	while (1)
 		{
-			char c1 = *s1;     // c1 - bukv, na kot ukaz s1
-			char c2 = *s2;     // c2 - bukv, na kot ukaz s2
-				if (c1 == '\0')    // esli dostignut konec slova, vyxodim iz cikla
+			char c1 = *s1;     
+			char c2 = *s2;     
+				if (c1 == '\0')    
 					return c1 - c2;
-				if (c1 != c2)      // esli bukvy raznye, vyxodim iz cikla
+				if (c1 != c2)      
 					return c1 - c2;
-			*s1++;             // dvigaem ukazatel na sleduyuschuyu bukvu  v 1-m slove
-		    *s2++;             // dvigaem ukazatel na sleduyuschuyu bukvu vo 2-m slove
+			*s1++;             
+		    *s2++;             
 		}
 }
-
-
 void qsort(char* uknaslo[], int che)
 {
-	
-	for (int j = 0; j < (che - 1); j++)// ne vyti za granicy mass
+	for (int j = 0; j < (che - 1); j++)
 	{
 		for (int i = 0; i < (che - 1); i++)
 		{
-			int result = comparesl(uknaslo[i], uknaslo[i + 1]); // vozvrashaet chislo 
-			//printf("%s %s  ->  %d \n", uknaslo[i], uknaslo[i+1], result);
+			int result = comparesl(uknaslo[i], uknaslo[i + 1]); 
 			char* temp;
-			if (result > 0) // nado pomenjat slova mestami
+			if (result > 0) 
 			{
 				temp = uknaslo[i];
 				uknaslo[i] = uknaslo[i + 1];
 				uknaslo[i + 1] = temp;
 			}
-			//printf("%s %s  changed! \n", uknaslo[i], uknaslo[i + 1]);
 		}
 	}
-
-
-
-
 }
-
-
-
-
 int main()
 {
 char mass[] = "does goodby.,. dog., cat car";
-char* uknaslo[100]; // ykazatel on single words 
-int che = 0;// number of words
+char* uknaslo[100];  
+int che = 0;
 uknaslo[0] = &mass[0];
 che++;
-for (int i = 0; mass[i] != '\0'; i++)// simvol konza stroki
-{
-	if ((mass[i] == ' ') || (mass[i] ==  '.') || (mass[i] == ','))
+	for (int i = 0; mass[i] != '\0'; i++)
 	{
-		mass[i] = '\0'; // razdel'aem stroky na otdeln slova
-		uknaslo[che] = &mass[i + 1]; // shob nachalo slovo c bukv a ne probela
-		che++;
+		if ((mass[i] == ' ') || (mass[i] ==  '.') || (mass[i] == ','))
+		{
+			mass[i] = '\0'; 
+			uknaslo[che] = &mass[i + 1]; 
+			che++;
+		}
 	}
-}
 printf("che=%d \n", che);
 qsort(uknaslo, che);
-	
-
-for (int i = 0; i < che; i++)
-{
-	if (uknaslo[i][0] != '\0')
+	for (int i = 0; i < che; i++)
+	{
+		if (uknaslo[i][0] != '\0')
 		printf("%s \n", uknaslo[i]);
-}
-
-
-
-
+	}
 }

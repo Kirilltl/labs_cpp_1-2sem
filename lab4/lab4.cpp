23c4b31792bf5549ceec588f6c628861a623c00d
Kirill Kyzmenko <71287749+Kirilltl@users.noreply.github.com>
2020-11-04 17:04:45 +0300

    zas

--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,53 +1,49 @@
 #include<stdio.h>
-int comparesl(char* s1, char* s2)
-{
-	while (1)
-	{
-		char c1 = *s1;
-		char c2 = *s2;
-		if (c1 == '\0')
-			return c1 - c2;
-		if (c1 != c2)
-			return c1 - c2;
-		*s1++;
-		*s2++;
-	}
+
+int len(char* a) {
+	int res = 0;
+	for (int i = 0; a[i] != '\0'; i++)
+		res++;
+	return res;
 }
-void qsort(char* uknaslo[], int che)
+
+void perevorot(char* b)
 {
-	for (int j = 0; j < (che - 1); j++)
+	for (int i = len(b) - 1; i >= 0; i--)
 	{
-		for (int i = 0; i < (che - 1); i++)
-		{
-			int result = comparesl(uknaslo[i], uknaslo[i + 1]);
-			char* temp;
-			if (result > 0)
-			{
-				temp = uknaslo[i];
-				uknaslo[i] = uknaslo[i + 1];
-				uknaslo[i + 1] = temp;
-			}
-		}
+		printf("%c", b[i]);
 	}
+	printf("\n");
 }
+
+
+
 int main()
 {
-	char mass[] = "does dog cat car";
+	char mass[] = "does goodby.,. dog., cat car";
 	char* uknaslo[100];
 	int che = 0;
 	uknaslo[0] = &mass[0];
 	che++;
 	for (int i = 0; mass[i] != '\0'; i++)
 	{
-		mass[i] = '\0';
-		uknaslo[che] = &mass[i + 1];
-		che++;
+		if ((mass[i] == ' ') || (mass[i] == '.') || (mass[i] == ','))
+		{
+			mass[i] = '\0';
+			uknaslo[che] = &mass[i + 1];
+			che++;
+		}
 	}
 	printf("che=%d \n", che);
-	qsort(uknaslo, che);
+
+
 	for (int i = 0; i < che; i++)
 	{
 		if (uknaslo[i][0] != '\0')
-			printf("%s \n", uknaslo[i]);
+		{
+			//printf("%s %d \n", uknaslo[i], len(uknaslo[i]));
+			perevorot(uknaslo[i]);
+		}
 	}
+	
 }

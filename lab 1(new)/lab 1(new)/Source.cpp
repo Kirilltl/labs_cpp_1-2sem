a0a64a3eae20495555ae305c7f9e173ac958f94c
Kirill Kyzmenko <71287749+Kirilltl@users.noreply.github.com>
2020-11-04 17:49:00 +0300

    add

--- lab 1(new)/lab 1(new)/Source.cpp
+++ lab 1(new)/lab 1(new)/Source.cpp
@@ -1,42 +1,29 @@
 # include <stdio.h>
-//# include <math.h>
-
-
 int floor(float a) 
 {
 	int res = (int)a;
-
 	if (a < 0)
 		res=a-1;
 	return res;
 }
-
-
-
-
 int main()
 {
+	float nums[10] = { -1.99, 2.565,-0.3, 2.34, -4.222, 3.45, 3.555,1.3, 1.367, 5.56 };
 	
-
-    float nums[10] = { -1.99, 2.565,-0.3, 2.34, -4.222, 3.45, 3.555,1.3, 1.367, 5.56 };
-        for (int i = 0; i < 10; i++)
-	  {
+    for (int i = 0; i < 10; i++)
+	{
+		printf(" The number i entered: %f \n ", nums[i]);
 		nums[i] = nums[i] - floor(nums[i]);
-		printf("drob. chast: %f \n",nums[i]);
-			
-
-
-      }
-                  // poisk minimal'noj drobnoj chasti
-	double min = 1;
+		printf(" Drob.chast of this number: %f \n ",nums[i]);
+	}
+    double min = 1;
 	for (int i = 0; i < 10; i++)
 	{
-
 		if (nums[i] < min)
 		{
 			min =  nums[i];
 		}
 	}
-	printf("min drob chast: %f", min);
+	printf(" Min drob.chast: %f ", min);
 	return 0;
 }
--- lab4/lab4.cpp
+++ lab4/lab4.cpp
@@ -1,49 +1,43 @@
 #include<stdio.h>
-
-int len(char* a) {
+int lengthword(char* a) 
+{
 	int res = 0;
 	for (int i = 0; a[i] != '\0'; i++)
 		res++;
 	return res;
 }
-
-void perevorot(char* b)
+void perevorotword(char* b)
 {
-	for (int i = len(b) - 1; i >= 0; i--)
+	for (int i = lengthword(b) - 1; i >= 0; i--)
 	{
 		printf("%c", b[i]);
 	}
 	printf("\n");
 }
-
-
-
 int main()
 {
-	char mass[] = "does goodby.,. dog., cat car";
+	char mass[] = " olleh esaelp evig em a sulp ";
 	char* uknaslo[100];
 	int che = 0;
 	uknaslo[0] = &mass[0];
 	che++;
 	for (int i = 0; mass[i] != '\0'; i++)
 	{
-		if ((mass[i] == ' ') || (mass[i] == '.') || (mass[i] == ','))
+		if (mass[i] == ' ') 
 		{
 			mass[i] = '\0';
 			uknaslo[che] = &mass[i + 1];
 			che++;
 		}
 	}
-	printf("che=%d \n", che);
-
-
+	printf(" che=%d \n ", che);
 	for (int i = 0; i < che; i++)
 	{
+		printf(" %s \n ", uknaslo[i]);
 		if (uknaslo[i][0] != '\0')
 		{
-			//printf("%s %d \n", uknaslo[i], len(uknaslo[i]));
-			perevorot(uknaslo[i]);
+			perevorotword(uknaslo[i]);
+			printf("\n");
 		}
 	}
-	
 }

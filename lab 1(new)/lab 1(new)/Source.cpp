0e9d3d389491adf724aafa552a50ca0d968aa932
Kirill Kyzmenko <71287749+Kirilltl@users.noreply.github.com>
2020-11-04 14:02:35 +0300

    file

--- lab 1(new)/lab 1(new)/Source.cpp
+++ lab 1(new)/lab 1(new)/Source.cpp
@@ -1,7 +1,23 @@
 # include <stdio.h>
-# include <math.h>
+//# include <math.h>
+
+
+int floor(float a) 
+{
+	int res = (int)a;
+
+	if (a < 0)
+		res=a-1;
+	return res;
+}
+
+
+
+
 int main()
 {
+	
+
     float nums[10] = { -1.99, 2.565,-0.3, 2.34, -4.222, 3.45, 3.555,1.3, 1.367, 5.56 };
         for (int i = 0; i < 10; i++)
 	  {

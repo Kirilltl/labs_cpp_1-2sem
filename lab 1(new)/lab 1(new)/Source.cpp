c6ec1240fa636d67e7e5f0eab8293836dbfb634c
Kirill Kyzmenko <71287749+Kirilltl@users.noreply.github.com>
2020-11-10 00:00:33 +0300

    modify files

--- lab 1(new)/lab 1(new)/Source.cpp
+++ lab 1(new)/lab 1(new)/Source.cpp
@@ -8,14 +8,16 @@ int floor(float a)
 }
 int main()
 {
-	float nums[10] = { -1.98, 2.565,-0.3, 2.34, -4.222, 3.45, 3.555,1.3, 1.367, 5.56 };
+float nums[10];
 	for (int i = 0; i < 10; i++)
 	{
-		printf(" The number i entered: %f \n ", nums[i]);
+		printf("Input number:");
+		scanf("%f", &nums[i]);
 		nums[i] = nums[i] - floor(nums[i]);
-		printf(" Drob.chast of this number: %f \n ",nums[i]);
+		printf("Drob.chast of this number: %f \n", nums[i]);
+		printf("\n");
 	}
-    double min = 1;
+double min = 1;
 	for (int i = 0; i < 10; i++)
 	{
 		if (nums[i] < min)
@@ -23,6 +25,6 @@ int main()
 			min =  nums[i];
 		}
 	}
-	printf(" Min drob.chast: %f ", min);
+	printf("Min drob.chast: %f", min);
 	return 0;
 }

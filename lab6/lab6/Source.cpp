0f2bba3c4fe5833e96e01e613cacc3516e661f92
Kirill Kyzmenko <71287749+Kirilltl@users.noreply.github.com>
2020-12-18 19:01:09 +0300

    fas

--- lab6/lab6/Source.cpp
+++ lab6/lab6/Source.cpp
@@ -63,85 +63,65 @@ void insert(List* list, Node* node)
     node->next = NULL;
     list->end = node;
 }
-
-
-// Создание упорядоченного списка на основе файла
 List* create_list(const char* file, int id)
 {
-    // открываем файл:
     FILE* fp;
-    if ((fp = fopen(file, "r")) == NULL) {
+    if ((fp = fopen(file, "r")) == NULL) 
+    {
         printf("Cannot open file '%s'\n", file);
         exit(1);
     }
-
-    // создаем пустой список:
     List* list = (List*)malloc(sizeof(List));
     list->start = NULL;
     list->end = NULL;
     list->id = id;
-
-    // читаем первый символ из файла:
     char c = fgetc(fp);
-
-    // в цикле читаем остальные символы из файла, определяем где начинается и заканчивается каждое слово,
-    // записываем слово в дин.память, помещаем слово в узел, а узел - в список:
-    int len = 0;           // длина слова и текущая позиция буквы в слове
-    int capacity = 1;      // размер памяти, выделенной под текущее слово
-    int first_letter = 1;  // флаг: является ли текущая буква первой в текущем слове?
-    int inserted = 0;     // флаг: вставленно ли текущее слово в список?
-    Node* node = NULL;     // текущий узел
+    int len = 0;          
+    int capacity = 1;      
+    int first_letter = 1;  
+    int inserted = 0;    
+    Node* node = NULL;    
     while (c != EOF)
     {
-        // если это первая буква в слове, то создаем новый узел:
         if (is_alpha(c) && first_letter)
         {
             len = 0;
             capacity = 1;
             inserted = 0;
             node = (Node*)malloc(sizeof(Node));
-            node->word = (char*)malloc(sizeof(char)); // выделяем 1 байт
-            node->word[len] = c;  // помещаем в память символ, прочитанный из файла
+            node->word = (char*)malloc(sizeof(char)); 
+            node->word[len] = c;  
             first_letter = 0;
             len++;
         }
-
-        // если это уже не первая буква в слове, то записываем ее в созданный узел:
         else if (is_alpha(c) && !first_letter)
         {
-            if (len >= capacity) { // если нужно, выделяем еще памяти
+            if (len >= capacity)
+            { 
                 capacity *= 2;
                 node->word = (char*)realloc(node->word, capacity * sizeof(char));
             }
-            node->word[len] = c; // помещаем в память символ, прочитанный из файла
+            node->word[len] = c; 
             len++;
         }
-
-        // если слово закончилось, то добавляем "\0" и вставляем созданный узел в список:
         else if (!is_alpha(c) && !first_letter && !inserted)
         {
-            if (len >= capacity) { // (если нужно) выделяем 1 байт для символа "\0"
+            if (len >= capacity) 
+            { 
                 node->word = (char*)realloc(node->word, capacity + 1);
             }
-            node->word[len] = '\0'; // помещаем в память символ "\0" - "конец строки"
+            node->word[len] = '\0'; 
             len++;
 
-            insert(list, node); // вставка узла в список
-            inserted = 1;       // между словами может быть несколько пробелов (или "плохих" сиволов). Этот флаг позволяет игнорировать их.
-            first_letter = 1;   // когда мы встретим обычную букву, то это означает начало нового слова.      
+            insert(list, node); 
+            inserted = 1;       
+            first_letter = 1;        
         }
-
-        // читаем следующий символ из файла:
         c = fgetc(fp);
     }
-
-    // закрываем файл:
     fclose(fp);
-
     return list;
 }
-
-// вывод списка на экран
 void print_list(List* list)
 {
     printf("List %d:\n", list->id);
@@ -150,76 +130,54 @@ void print_list(List* list)
         printf("Node->word: '%s'\n", n->word);
     }
 }
-
-// Объединение списков. Узлы списка "src" перемещаются в список "dst" 
 void merge(List* dst, List* src)
 {
-    Node* n = src->start; // текущий узел
+    Node* n = src->start;
     while (n != NULL)
     {
-        Node* node = n; // запоминаем текущий узел
-        n = n->next;    // переходим к следующему
-
-        insert(dst, node); // Вставляем имеено "node". Если бы мы вставили узел "n", то insert() поменял бы его указатель "n->next", и на следующей итерации мы получили бы ошибку 
+        Node* node = n; 
+        n = n->next;    
+        insert(dst, node); 
     }
-
-    // список "src" теперь пуст:
     src->start = NULL;
     src->end = NULL;
 }
-
-
-// Удаление списка
 void delete_list(List* list)
 {
     printf("Delete list %d\n", list->id);
-
-    // сначала удаляем узлы списка, один за другим:
-    Node* n = list->start; // текущий узел
+    Node* n = list->start; 
     while (n != NULL)
     {
-        Node* node = n;  // запоминаем текущий узел
-        n = n->next;     // переходим к следующему
-
+        Node* node = n;  
+        n = n->next;     
         printf("  delete '%s'\n", node->word);
         free(node->word);
-        free(node);       // Удаляем имеено узел "node". Если бы мы удалили узел "n", на следующей итерации мы не смогли бы перейти к следующему узлу (n->next)
+        free(node);       
     }
-
-    // удаляем пустой список:
     free(list);
 }
-
-
-
 int main()
 {
-    // создаем списки
     List* one = create_list("C:\\github\\labs\\lab6\\Debug\\one.txt", 1);
     List* two = create_list("C:\\github\\labs\\lab6\\Debug\\two.txt", 2);
     List* three = create_list("C:\\github\\labs\\lab6\\Debug\\three.txt", 3);
 
-    // выводим списки на экран
     print_list(one);
     print_list(two);
     print_list(three);
-
-    // объединяем списки ("one" становится одним большим списком, а "two" и "three" - пустыми)
+    
     merge(one, two);
     merge(one, three);
 
-    // 	выводим списки на экран
-    printf("\nAfter merge:\n");
+    printf("\n After merge:\n");
     print_list(one);
     print_list(two);
     print_list(three);
 
-    // удаляем списки 
     printf("\n");
     delete_list(one);
     delete_list(two);
     delete_list(three);
-
     return 0;
 }
 

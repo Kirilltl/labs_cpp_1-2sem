ac91bac9d3c00621db83863ee1030ed2d7703746
Kirill Kyzmenko <71287749+Kirilltl@users.noreply.github.com>
2021-03-27 17:43:38 +0300

    pass

--- lab8d/lab8d/func.h
+++ lab8d/lab8d/func.h
@@ -1,1 +1,6 @@
-#pragma once
+/*
+#ifndef _FUNC_H_
+#define _FUNC_H_
+int return_response();
+#endif
+*/
---
+++ lab8d/lab8d/main.cpp
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+//#include "func.h"
+int main()
+{
+	int mass[2][2] = {{4, 8},{9, -1}};//stroka stolbec
+	for (int i = 0; i < 2; i++)
+	{
+		for (int j = 0; j < 2; j++)
+		{
+			//mass[i][j] = i * j;
+			printf("%2d", mass[0][1]);
+		}
+		printf("\n");
+	}
+
+	
+	
+	
+	
+	
+	//int index = return_response();
+	//printf("Index: %d \n", index);
+}

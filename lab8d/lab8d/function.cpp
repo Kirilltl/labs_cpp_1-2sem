2b71d0073233a824379515986a6aa749ed9130e1
Kirill Kyzmenko <71287749+Kirilltl@users.noreply.github.com>
2021-03-31 19:42:07 +0300

    remake this lab

--- lab8d/lab8d/function.cpp
+++ lab8d/lab8d/function.cpp
@@ -3,14 +3,15 @@
 #include<math.h>
 double find_deviation(double MAX_I, double MAX_J)
 {
-	double** mass = (double**)malloc(MAX_I*sizeof(double*));
-	for (int i = 0; i < MAX_I; ++i)
+	double** mass = NULL;
+	mass = (double**)malloc(MAX_I*sizeof(double*));
+	for (int i = 0; i < MAX_I; i++)
 		mass[i] = (double*)malloc(MAX_J*sizeof(double));
 		for (int i = 0; i < MAX_I; i++)
 		for (int j = 0; j < MAX_J; j++)
 		{
 			printf("mass[%d][%d]=", i, j);
-			int r = scanf("%lf",&mass[i][j]);
+			int r = scanf("%lf", &mass[i][j]);
 		}
 	double res = 0;
 	for (int i = 0; i < MAX_I; i++)
